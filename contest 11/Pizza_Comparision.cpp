67be2e819b105bc5d9deca8c0cba43531f17fc43
maksudurrahmanmafi <maksudurrahmanmafi0@gmail.com>
2026-01-09 21:34:16 +0600

    Richie rich

--- contest 11/Pizza_Comparision.cpp
+++ contest 11/Pizza_Comparision.cpp
@@ -2,19 +2,24 @@
 using namespace std;
 #define ll long long int
 
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int t;
-    cin>>t;
-    while(t--){
-     int a,b;
-     cin>>a>>b;
-     double small = 100.0/a;
-     double large = 225.0/b;
-     if(small>large)cout<<"Small\n";
-     else if(large>small)cout<<"Large\n";
-     else cout<<"Equal\n";
+    cin >> t;
+    while (t--)
+    {
+        int a, b;
+        cin >> a >> b;
+        double small = 100.0 / a;
+        double large = 225.0 / b;
+        if (small > large)
+            cout << "Small\n";
+        else if (large > small)
+            cout << "Large\n";
+        else
+            cout << "Equal\n";
     }
     return 0;
 }
