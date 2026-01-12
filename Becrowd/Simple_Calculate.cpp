da7325422034d6978a0a110e89e87ee85aa3449c
maksudurrahmanmafi <maksudurrahmanmafi0@gmail.com>
2026-01-12 18:57:50 +0600

    Codechef and codeforcess

---
+++ Becrowd/Simple_Calculate.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int a,b,c,d;
+    double e,f;
+    cin>>a>>b>>e>>c>>d>>f;
+    cout<<fixed<<setprecision(2)<<"VALOR A PAGAR: R$ "<<(b*e)+(d*f);
+    return 0;
+}
