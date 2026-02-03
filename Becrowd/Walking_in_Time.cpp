3fa30a5f862c0f9abe3b32843839e10fbb83c973
maksudurrahmanmafi <maksudurrahmanmafi0@gmail.com>
2026-02-03 21:31:20 +0600

    Walking

---
+++ Becrowd/Walking_in_Time.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B, C;
+    cin >> A >> B >> C;
+
+    if (A == B || A == C || B == C ||
+        A + B == C || A + C == B || B + C == A) {
+        cout << "S\n";
+    } else {
+        cout << "N\n";
+    }
+
+    return 0;
+}
