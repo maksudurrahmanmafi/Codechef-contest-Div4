3df8e3464b37f6a19ed40b197d6237e5384c3717
maksudurrahmanmafi <maksudurrahmanmafi0@gmail.com>
2026-02-13 21:38:44 +0600

    Insect

---
+++ codechef 1* to 2*/Insect.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--){
+     int x;
+     cin>>x;
+     cout<<(x>8000?"Mais de 8000!\n":"Inseto!\n");
+    }
+    return 0;
+}
