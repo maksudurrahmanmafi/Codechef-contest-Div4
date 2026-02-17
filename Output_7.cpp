ee038ef084c06c971a6afa4060899d2c7cfe18cc
maksudurrahmanmafi <maksudurrahmanmafi0@gmail.com>
2026-02-17 18:47:44 +0600

    Output 7

---
+++ Output_7.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    for(int i = 97;i<=122;i++){
+        cout<<i<<" e "<<char(i)<<'\n';
+    }
+    return 0;
+}
