4b2188e1639cc0f19d32a6ac31c4b96d7549a55c
maksudurrahmanmafi <maksudurrahmanmafi0@gmail.com>
2026-02-19 18:37:15 +0600

    Merry

---
+++ Merry_Christmaaas.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin >> n;
+    cout << "Feliz nat";
+
+    for (int i = 0; i < n; i++)
+    {
+        cout<<'a';
+    }
+    cout<<"l!"<<'\n';
+    return 0;
+}
