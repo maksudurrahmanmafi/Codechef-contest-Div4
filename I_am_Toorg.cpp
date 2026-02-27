ec90736f1daccd167e42983beb063ece23964486
maksudurrahmanmafi <maksudurrahmanmafi0@gmail.com>
2026-02-27 15:51:04 +0600

    Toorg

---
+++ I_am_Toorg.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--){
+     string s;
+     getline(cin,s);
+     cout<<"I am Toorg!\n";
+    }
+    return 0;
+}
