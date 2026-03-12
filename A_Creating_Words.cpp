7c32896eb49c56f5e2363c56cf760e77907b3e61
maksudurrahmanmafi <maksudurrahmanmafi0@gmail.com>
2026-03-12 09:20:40 +0600

    Word

---
+++ A_Creating_Words.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--){
+     string a,b;
+     cin>>a>>b;
+     swap(a[0],b[0]);
+     cout<<a<<" "<<b<<endl;
+    }
+    return 0;
+}
