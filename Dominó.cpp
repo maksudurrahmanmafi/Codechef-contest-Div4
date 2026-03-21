c5599b851aaf8a1a63cdcac957f43619c5cada64
maksudurrahmanmafi <maksudurrahmanmafi0@gmail.com>
2026-03-21 23:55:12 +0600

    Domino

---
+++ Dominó.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+
+    long long result = (n + 1) * (n + 2) / 2;
+
+    cout << result << '\n';
+
+    return 0;
+}
