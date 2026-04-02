475176f241ddbe71d08acb4b4e431fa06772e97b
maksudurrahmanmafi <maksudurrahmanmafi0@gmail.com>
2026-04-02 20:27:10 +0600

    Overspeed

---
+++ Overspeeding.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int x;
+    cin>>x;
+    cout<<(x<=40?"NO":"YES")<<'\n';
+    return 0;
+}
