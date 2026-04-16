49ae6ff7f043bf81940d69c4381779e20a23cb90
maksudurrahmanmafi <maksudurrahmanmafi0@gmail.com>
2026-04-16 20:18:22 +0600

    Forgotten note

---
+++ Forgotten_Note.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int x,y;
+    cin>>x>>y;
+    cout<<(y*2-x)<<endl;
+    return 0;
+}
