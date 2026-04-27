59cd16b452669d7d072c7d6103b7589090741646
maksudurrahmanmafi <maksudurrahmanmafi0@gmail.com>
2026-04-27 20:18:21 +0600

    Wheel

---
+++ Giant_Wheel.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int x;
+    cin>>x;
+    cout<<(x>=60?"Yes":"No");
+    return 0;
+}
