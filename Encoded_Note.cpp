cf5e853be94f2e005015b335cf5553fb2d5338e1
maksudurrahmanmafi <maksudurrahmanmafi0@gmail.com>
2026-04-30 20:11:00 +0600

    Encoded note

---
+++ Encoded_Note.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    while(cin>>t){
+        cout<<char('a'+(t-1))<<endl;
+    }
+    return 0;
+}
