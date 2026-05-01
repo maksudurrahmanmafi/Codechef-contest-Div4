83712c5ff2ec1bc23beaf2198fd3f8f923d614f7
maksudurrahmanmafi <maksudurrahmanmafi0@gmail.com>
2026-05-01 19:46:40 +0600

    Party

---
+++ Parity.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--){
+     int i ;
+     cin>>i;
+     cout<<(i%2==0?"Yes\n":"No\n");
+    }
+    return 0;
+}
