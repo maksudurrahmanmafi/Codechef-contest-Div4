5251cc2ba1f228ed23b71324d76401363d9f546a
maksudurrahmanmafi <maksudurrahmanmafi0@gmail.com>
2026-05-21 09:20:46 +0600

    Sum

---
+++ Sum_it.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--){
+     int a,b,c;
+     cin>>a>>b>>c;
+     if(a+b==c)cout<<"YES\n";
+     else cout<<"NO\n";
+    }
+    return 0;
+}
