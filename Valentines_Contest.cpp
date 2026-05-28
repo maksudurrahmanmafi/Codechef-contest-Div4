d8e162b10b556630cbc355a6026d6adf53bbcd84
maksudurrahmanmafi <maksudurrahmanmafi0@gmail.com>
2026-05-28 18:53:17 +0600

    Valentines

---
+++ Valentines_Contest.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int x;
+    cin>>x;
+    cout<<(x==121?"Likely":"Unlikely");
+    return 0;
+}
