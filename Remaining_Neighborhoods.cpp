f37e18b7cdebe4e1712e73cce527cfa22346caab
maksudurrahmanmafi <maksudurrahmanmafi0@gmail.com>
2026-06-01 06:00:15 +0600

    Neighborhoods

---
+++ Remaining_Neighborhoods.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+     int x;
+     cin>>x;
+     cout<<100-x<<endl;
+
+
+    return 0;
+}
