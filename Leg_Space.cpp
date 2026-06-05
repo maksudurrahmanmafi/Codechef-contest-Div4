8ef8b0ebeddffadcf8c4031474779dab1f18a9bd
maksudurrahmanmafi <maksudurrahmanmafi0@gmail.com>
2026-06-05 20:19:22 +0600

    Leg space

---
+++ Leg_Space.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n,m;
+    cin>>n>>m;
+    cout<<(n<m?"YES":"NO");
+    return 0;
+}
