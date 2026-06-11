8f00f7495cfeaee561b2bd03888c92c127705d95
maksudurrahmanmafi <maksudurrahmanmafi0@gmail.com>
2026-06-11 11:05:39 +0600

    Run chase

---
+++ Run_Chase.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int x;
+    cin>>x;
+    cout<<x/20+1<<endl;
+}
